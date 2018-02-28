c7eee4241ddef6254a2f693729e3690e38316f92
mone27 <mone.massaro@gmail.com>
2018-02-28 19:44:46 +0100

    put a random word in the crossword

--- gnuciverba.py
+++ gnuciverba.py
@@ -12,7 +12,7 @@ import logging
 import numpy as np # not rally necessary but it is nice to use it :D
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 
 class LoadingWidget:
@@ -62,18 +62,54 @@ class LoadingWidget:
 class Gnuciverba:
 
     def __init__(self, x: int, y: int, dict_file):
-        # generate random x*y matrix
-        self.crossword = np.array([[random.choice(string.ascii_lowercase) for _ in range(x)] for _ in range(y)])
+        self.crossword = np.empty((x, y), dtype='<U1')
+        self.crossword[:,:] = '#'  # init matrix
+        self.x = x
+        self.y = y
         with open(dict_file) as f:
-            self.dict = {i for i in f.read().splitlines() if len(i) <= min(x, y)}  # removes words that are too long
+            self.dict = np.array([i for i in f.read().splitlines() if len(i) <= min(x, y)])  # removes words that are too long
 
         log.debug(f"using this dictionary: {self.dict}")
-        self._write_on_row("ciao")
-        self._write_on_column("bene")
 
-        # it takes a random word and put it in a random position
-    def _write_on_column(self, word, start=(0,0)):  # maybe add type notations
-        self.crossword[start[0]:len(word), start[1]] = list(word)
+    def generate(self):
+        # put a random word in the crossword
+        # np.random.seed(1)  # maybe not the best place
+        # for _ in range(self.x):
+        while not self._write_word_column(np.random.choice(self.dict)):
+            print(self)
+
+        self._write_on_column("ciao")
+
+    def _write_word_column(self, word ):
+        # need to add randomness
+        for column in range(self.y):
+            for row in range((self.x-len(word)+1)): # every position in the row
+                if self._write_on_column(word,(row,column)):
+                    return True
+        return False
+
+
+
+    def _write_on_column(self, word, start=(0,0)):
+        # TODO write unit tests for this mess
+        # check that the word can be replaced
+        if len(word) + start[0]  > self.x:
+            return False # maybe also the type of the error
+        old_word = self.crossword[start[0]:len(word), start[1]]
+
+        if self._can_put_string(old_word, word):  # if there is no collision replace the word
+            self.crossword[start[0]:len(word), start[1]] = list(word)
+            return True
+        else:
+            return False # if there is a collision so it cannot replace the word
+
+
+    def _can_put_string(self, old_word, new_word):
+        for c1,c2 in zip(old_word, new_word):
+           if c2 != c1 and c1 != '#':
+               return False
+        return True
+
 
     def _write_on_row(self, word, start=(0,0)):
         self.crossword[start[0], start[1]:len(word)] = list(word)
@@ -86,10 +122,11 @@ class Gnuciverba:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)  # !DEBUG
 
     with LoadingWidget():
         gnu = Gnuciverba(5, 5, "1000_parole_italiane_comuni.txt")
+        gnu.generate()
     print(gnu)
 
     print("congratulation now you have to find the sense of this crossword")
